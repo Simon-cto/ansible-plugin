61b4336553cc672e4dc7846c70b0cd13a02a495c
SimonC <2318325+Simon-cto@users.noreply.github.com>
2023-09-28 11:45:42 +0200

    Update AnsibleRunnerBuilder.java

--- src/main/groovy/com/rundeck/plugins/ansible/ansible/AnsibleRunnerBuilder.java
+++ src/main/groovy/com/rundeck/plugins/ansible/ansible/AnsibleRunnerBuilder.java
@@ -688,7 +688,7 @@ public class AnsibleRunnerBuilder {
 		Set<ContextView> sharedDataContextKeys = getContext().getSharedDataContext().consolidate().getKeys();
 		Iterator keysIterator = sharedDataContextKeys.iterator();
 		while(keysIterator.hasNext()){
-			ContextView key = keysIterator.next();
+			Object key = keysIterator.next();
 			extraVars += System.lineSeparator() + key.toString() + ": \"" + getContext().getSharedDataContext().consolidate().get(key).toString() + "\"";
 		}
 		// extraVars += System.lineSeparator() + "shared_context: \"" + getContext().getSharedDataContext().consolidate().getData().toString() +"\"";
