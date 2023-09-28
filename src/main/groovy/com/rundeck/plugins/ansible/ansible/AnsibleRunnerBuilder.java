871ca3f5c425cae4458dea6a85298ed6d554739d
SimonC <2318325+Simon-cto@users.noreply.github.com>
2023-09-28 10:19:33 +0200

    Update AnsibleRunnerBuilder.java

--- src/main/groovy/com/rundeck/plugins/ansible/ansible/AnsibleRunnerBuilder.java
+++ src/main/groovy/com/rundeck/plugins/ansible/ansible/AnsibleRunnerBuilder.java
@@ -682,7 +682,7 @@ public class AnsibleRunnerBuilder {
 	extraVars += System.lineSeparator() + "data_context: " + getContext().getDataContextObject().toString();
 	extraVars += System.lineSeparator() + "private_context: " + getContext().getPrivateDataContextObject().toString();
 	try{
-		extraVars += System.lineSeparator() + "shared_context: " + getContext().getSharedDataContext().consolidate().getClass().getCanonicalName();
+		extraVars += System.lineSeparator() + "shared_context: " + getContext().getSharedDataContext().getClass().getCanonicalName();
 	}
 	catch (Exception e){
 		extraVars += System.lineSeparator() + "shared_context: null";
